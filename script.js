f98f6ed926e4b1e874ae27bf481580e0894e513e
First Last <salmanashraf853@gmail.com>
2019-09-03 22:52:55 +0100

    s

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const aud = document.querySelector('audio')
 aud.addEventListener('play', ()=>{
     console.log(3333333)
     document.querySelector('.top img').classList.add('roll')
+    document.querySelector('.o').classList.add('oo')
 })
 aud.addEventListener('pause', ()=>{
     document.querySelector('.top img').classList.remove('roll')
